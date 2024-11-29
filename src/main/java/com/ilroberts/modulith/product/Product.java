9321d5690aa8b3e4c4a4b7d47e346a93783d8df4
Iwan Roberts <iwanr@kainos.com>
2024-11-29 17:01:35 +0000

    everything working again

--- src/main/java/com/ilroberts/modulith/product/Product.java
+++ src/main/java/com/ilroberts/modulith/product/Product.java
@@ -2,6 +2,11 @@ package com.ilroberts.modulith.product;
 
 import jakarta.persistence.Column;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,13 +15,17 @@ import org.jmolecules.ddd.types.AggregateRoot;
 
 import java.math.BigDecimal;
 
+@Entity
 @ToString(callSuper = true)
 @Builder
 @Getter
 @AllArgsConstructor
-public class Product implements AggregateRoot<Product, ProductId> {
+public class Product {
 
-    private final @Column(unique = true) ProductId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_seq")
+    @SequenceGenerator(name = "product_id_seq", sequenceName = "product_id_seq", allocationSize = 1)
+    private final @Column(unique = true) Long id;
 
     private final String name;
     private final String description;
--- src/main/java/com/ilroberts/modulith/product/Products.java
+++ src/main/java/com/ilroberts/modulith/product/Products.java
@@ -1,6 +1,8 @@
 package com.ilroberts.modulith.product;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface Products extends CrudRepository<Product, ProductId> {
 }
