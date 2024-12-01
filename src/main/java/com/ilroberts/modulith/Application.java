01763355cb4876d06c7c56c4def6839daeb0bb04
Iwan Roberts <iwanr@kainos.com>
2024-12-01 10:59:04 +0000

    added annotation to main application

--- src/main/java/com/ilroberts/modulith/Application.java
+++ src/main/java/com/ilroberts/modulith/Application.java
@@ -3,7 +3,9 @@ package com.ilroberts.modulith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.modulith.Modulithic;
 
+@Modulithic
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
