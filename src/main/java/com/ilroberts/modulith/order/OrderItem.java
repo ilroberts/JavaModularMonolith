99d442c7fa3aec12ebed80b40b654a997c6e57d3
Iwan Roberts <iwanr@kainos.com>
2024-12-03 19:32:53 +0000

    fixed unit test failures

--- src/main/java/com/ilroberts/modulith/order/OrderItem.java
+++ src/main/java/com/ilroberts/modulith/order/OrderItem.java
@@ -3,6 +3,7 @@ package com.ilroberts.modulith.order;
 import lombok.Builder;
 import org.jmolecules.ddd.annotation.Entity;
 import org.jmolecules.ddd.annotation.Identity;
+import org.jmolecules.ddd.annotation.ValueObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -10,5 +11,5 @@ import java.math.BigDecimal;
 
 @Builder
 @Table("ORDER_ITEM")
-@Entity
+@ValueObject
 public record OrderItem(Long productId, int quantity, BigDecimal price) {}
